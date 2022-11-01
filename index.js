70ee707b1bbe6d6ef52f922cd5c6f6e63ff5d795
abc <abc@xyz>
2022-11-01 11:07:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Import your express package
+const express = require("express");
+//Import mongodb driver
+const { MongoClient } = require("mongodb");
+//Initialize one express instance
+const server = express();
+
+//Add one route for the server to listen to
+server.use(express.json());
+
+server.post("/add-todo", async (req, res) => {
+    const { title } = req.body;
+    console.log(title);
+    const client = new MongoClient("mongodb+srv://cse:csesmit123@cluster0.udrw5uh.mongodb.net/?retryWrites=true&w=majority");
+    await client.connect();
+    const db = client.db("todo");
+    const collection = db.collection("todo-items");
+    await collection.insertOne({ title: title });
+    res.json({ message: "Success" })
+});
+
+server.get("/todo-items", async (req, res) => {
+    const client = new MongoClient("mongodb+srv://cse:csesmit123@cluster0.udrw5uh.mongodb.net/?retryWrites=true&w=majority");
+    await client.connect();
+    const db = client.db("todo");
+    const collection = db.collection("todo-items");
+    const data = await collection.find().toArray();
+    res.json(data);
+})
+
+server.get("/", (req, res) => {
+    res.json({ message: "Hello from server" })
+})
+
+//Listen on port 3000
+server.listen(3000, () => {
+    console.log("Server started")
+})
